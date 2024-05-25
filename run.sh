70d22b8666a964d05d12f982f4e2208000f27f41
HWG <haowei920@gmail.com>
2024-05-25 09:23:00 +0000

    Can get bytes from interface

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# creates the new rust binary
+cargo b --release
+# set permission such that binary has permission
+sudo setcap cap_net_admin=epi /home/hugoh/Desktop/rust/trust2/target/release/trust2
+# run binary in background
+/home/hugoh/Desktop/rust/trust2/target/release/trust2 &
+#get running process of binary's pid
+pid=$!
+
+sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip link set up dev tun0
+wait $pid
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() -> io::Result<()>{
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("Failed to cr");
+    let mut buf = [0u8; 1504];
+
+    let nbytes = nic.recv(&mut buf [..])?;
+    eprintln!("read {} bytes {:x?}", nbytes, &buf[..nbytes]);
+    Ok(())
+}
